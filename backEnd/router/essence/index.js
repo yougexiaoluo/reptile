d93845ca77260e6b7c7a2a07648d996f49d36bce
mr_luo <mr_luo2016@163.com>
2019-10-31 18:16:11 +0800

    实现简易的页面数据

--- backEnd/router/essence/index.js
+++ backEnd/router/essence/index.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const router = express.Router()
-const https = require('https')
+const request = require('request')
 
-let BaseUrl = 'https://www.vue-js.com/'
+let BaseUrl = 'https://www.vue-js.com'
 
 router.get('/', (req, res) => {
-  let query = req.url.split('?')[1]
+  let tab = req.query && req.query.tab
+  let queryNames = ['good', 'weex', 'share', 'ask', 'job']
+
+  if (queryNames.indexOf(tab) == -1) {
+    return res.send({ code: 404, msg: '页面不存在', success: false })
+  }
 
   // 存在query的时候在拼接
-  if (query) {
-    BaseUrl = `BaseUrl/?${query}`
+  if (tab) {
+    BaseUrl = `${BaseUrl}/?tab=${tab}`
   }
+
+  request.get(BaseUrl, function (error, response, body) {
+    if (response.statusCode == 200) {
+      console.log('body:', body); // Print the HTML for the Google homepage.
+      res.send({ code: 1, msg: '请求成功', success: true })
+    }
+  })
+
 
 })
 
